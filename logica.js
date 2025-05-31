278243ca97f6c7acd541de96e64d613ee99618b5
Leandro Carone Lanschi <leandrocala2@gmail.com>
2025-05-31 14:49:14 -0300

    [feat] Inserção de imagens

--- logica.js
+++ logica.js
@@ -1,4 +1,19 @@
 
 
+
+
+
+
+let arrayImagensAnimais = ["/img/Fotos animais 1366x768/1.jpg", "/img/Fotos animais 1366x768/2.jpg", "/img/Fotos animais 1366x768/3.jpg", 
+"/img/Fotos animais 1366x768/4.jpg", "/img/Fotos animais 1366x768/5.jpg", "/img/Fotos animais 1366x768/6.jpg", "/img/Fotos animais 1366x768/7.jpg"];
+
+function alteraImagem() {
+
+let indiceImagem = Math.floor(Math.random() * arrayImagensAnimais.length);
+
 let tagImg = document.querySelector(".slideShow");
-tagImg.src = "/img/1bela-praia-tropical-mar-e-oceano-com-palmeira-de-coco-na-hora-nascer-sol.jpg";
+tagImg.src = arrayImagensAnimais[indiceImagem];
+
+}
+
+setInterval(alteraImagem, 2000);
